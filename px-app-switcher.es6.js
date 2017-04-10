7345fa6b084dd88eb8dcc5e06db160e847cc9d57
Heather McManigal <heather.mcmanigal@ge.com>
2017-04-10 10:17:52 -0400

    Code clean up and style updates.

--- px-app-switcher.es6.js
+++ px-app-switcher.es6.js
@@ -2,17 +2,34 @@
   Polymer({
 
     is: 'px-app-switcher',
-    ready: function() {
-      var distributed = this.firstElementChild;
-      var rect = distributed.getBoundingClientRect();
-      console.log(rect.top, rect.right, rect.bottom, rect.left);
+    behaviors: [
+      Polymer.IronResizableBehavior
+    ],
+    /**
+     * attaches listeners when popover is created
+     *
+     * @method attached
+     */
+    attached: function() {
+      // Enable document-wide tap recognizer.
+      Polymer.Gestures.add(document, 'tap', null);
+      this._created();
+    },
+    /**
+     * removes listeners when popover is destroyed
+     *
+     * @method detached
+     */
+    detached: function () {
+      this.unlisten(this.firstElementChild.childNodes[1], 'tap', '_clickButtonEvent');
+      this.unlisten(document, 'click', '_clickButtonEvent');
     },
     properties: {
       /**
-      * This property keeps track of the number of clicks.
-      *
-      * @property counterValue
-      */
+       * This property keeps track of the number items in the main list.
+       *
+       * @property counterValue
+       */
       items: {
         type: Array,
         notify: true,
@@ -21,9 +38,9 @@
         }
       },
       /**
-       * This property keeps track of the number ofß clicks.
+       * This property keeps track of the icons list.
        *
-       * @property counterValue
+       * @property iconsList
        */
       iconsList: {
         type: Array,
@@ -31,39 +48,124 @@
           return [];
         },
         computed: '_computedItems(items, items.*)'
+      },
+      /**
+       * This property keeps track of the boolean for open and closing of the popover.
+       *
+       * @property open
+       */
+      open: {
+        type: Boolean,
+        value: false
+      },
+      /**
+       * This property keeps track the display string for open or closing css.
+       *
+       * @property displayString
+       */
+      displayString :{
+        type: String,
+        notify: true,
+        value: ''
       }
     },
     /**
-    * Handles click on the element defined in 'on-click' on the template.
-    *
-    * @method handleClick
-    */
+     * Adds a listener for resizing the window.
+     *
+     * @method handleClick
+     */
+    listeners: {
+      'iron-resize': '_onIronResize'
+    },
+
+    /**
+     * Adds listener events to document and element.
+     *
+     * @method created
+     */
+    _created: function () {
+      this.listen(this.firstElementChild.childNodes[1], 'tap', '_clickButtonEvent');
+      this.listen(document, 'click', '_clickButtonEvent');
+    },
+
+    /**
+     * Parses an array of strings into an iterative array for the html template.
+     *
+     * @method computedItems
+     */
     _computedItems: function (items) {
       if (this.items) {
         var computedItemsArr = [];
-        if (typeof this.items[0] === 'string') {
-          var len = items.length,
-            i = 0;
-          for (i; i < len; i++) {
-            if (this.checkboxMode) {
-              //default unchecked if using a string array in check mode
-              computedItemsArr.push({val: items[i], checked: false});
-            }
-            else {
-              computedItemsArr.push({val: items[i]});
-            }
-          }
-          return computedItemsArr;
-        }
-        else {
-          //with new array it looks like we need to do a copy of items
-          // seems like items can be sparse at this point
           items.forEach(function (item, index) {
             computedItemsArr.push(item);
           });
           return computedItemsArr;
+      }
+    },
+
+    /**
+     * Handles click on the element to either hide or show popover.
+     *
+     * @method handleButtonEvent
+     */
+    _clickButtonEvent: function (event) {
+      if(event.type === 'click' && !event.target.classList.contains("menu-button")) {
+        this._openOrHideElement(false, '');
+      } else if(event.type === 'tap' && event.target.classList.contains("menu-button")) {
+        if (this.open){
+          this._openOrHideElement(false, '');
+        } else {
+          this._setPosition();
+          this._openOrHideElement(true, 'open');
         }
       }
     },
+
+    /**
+     * Handles how to show ors hide the element on off click/ or secondary button click.
+     *
+     * @method hide
+     */
+    _openOrHideElement: function(displayBoolean, displayString) {
+      this.open = displayBoolean
+      this.displayString = displayString;
+    },
+
+    /**
+     * Handleshow to display popover element on show.
+     *
+     * @method setPosition
+     */
+    _setPosition: function() {
+      var bodyRect = this.firstElementChild.getBoundingClientRect();
+      var button = this.firstElementChild.childNodes[1].getBoundingClientRect();
+
+      if(button.right >  + window.innerWidth / 2) {
+        var right = ((bodyRect.right - button.right) + (button.width)) + 15;
+        this.customStyle['--shared-position-right'] = right + 'px !important;';
+        this.customStyle['--shared-position-left'] = '';
+        this.customStyle['--shared-margin'] = '50px';
+        this.customStyle['--carat-right'] = '10px !important;';
+        this.customStyle['--carat-left'] = '';
+      } else {
+        this.customStyle['--shared-position-left'] = button.left + 'px !important;';
+        this.customStyle['--shared-position-right'] = '';
+        this.customStyle['--shared-margin'] = '20px';
+        this.customStyle['--carat-right'] = '';
+        this.customStyle['--carat-left'] = '10px !important;';
+      }
+      this.updateStyles();
+    } ,
+
+    /**
+     * Handles how to position popover on screen resize
+     *
+     * @method onIronResize
+     */
+    _onIronResize: function () {
+      if(this.open) {
+        this._setPosition();
+      }
+    }
   });
 })();
